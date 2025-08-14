438fee896389ef6e422866cb74c467233b23f1da
徐添翼 <xutianyi12@outlook.com>
2025-08-14 09:50:24 +0800

    Update Components

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,15 +15,15 @@ use Wisdech\SMSVerify\Exception\SMSException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
-        commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
         $middleware->encryptCookies();
         $middleware->redirectUsersTo('/apps');
-        $middleware->redirectGuestsTo(fn($request) => redirectToLogin($request));
+        $middleware->redirectGuestsTo(fn ($request) => redirectToLogin($request));
 
         $middleware->web(append: [
             HandleInertiaRequests::class,
@@ -57,7 +57,7 @@ return Application::configure(basePath: dirname(__DIR__))
                 return apiResponse($e->getTrace(), $e->getMessage(), -1, API_RESPONSE_ERROR);
             } else {
 
-                $inertiaEnv = !app()->environment(['local', 'testing']);
+                $inertiaEnv = ! app()->environment(['local', 'testing']);
                 $inertiaStatus = in_array($response->getStatusCode(), [500, 503, 404, 403, 419]);
 
                 if ($inertiaEnv && $inertiaStatus) {
